c5396c9a8848dcafd5b5e4136c42be6fdb0585d0
tejaavrsk <teja.avrsk@gmail.com>
2018-05-21 01:14:15 +0000

    tenth

--- helm-init.sh
+++ helm-init.sh
@@ -1,6 +1,6 @@
 HELM_URL=https://storage.googleapis.com/kubernetes-helm
 HELM_TARBALL=helm-v2.4.1-linux-amd64.tar.gz
 curl -O $HELM_URL/$HELM_TARBALL
-tar xzfv $HELM_TARBALL -C /home/jenkins && rm $HELM_TARBALL
+tar xzfv $HELM_TARBALL -C /var/lib/jenkins/ && rm $HELM_TARBALL
 export PATH=/var/lib/jenkins/linux-amd64/:$PATH
 helm init --client-only
